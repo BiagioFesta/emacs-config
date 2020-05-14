2f604a989cca8553eef7039ea10dc3720b0e8bbd
BiagioFesta <biagio.festa@gmail.com>
2020-05-14 11:52:05 +0100

    Added jedi and pipenv support

--- etc/bf-config-python.el
+++ etc/bf-config-python.el
@@ -10,9 +10,27 @@
   (use-package pyvenv
     :ensure t))
 
+(defun bf-config-python--pipenv ()
+  "Install and configure package `pipenv'."
+  (use-package pipenv
+    :ensure t))
+
+(defun bf-config-python--company-jedi ()
+  "Install and configure package `company-jedi'."
+  (use-package company-jedi
+    :ensure t
+    :init
+    (setq jedi:use-shortcuts t)
+    :config
+    (push 'company-jedi company-backends)
+    :hook
+    (python-mode . jedi:setup)))
+
 (defun bf-config-python ()
   "Setup environment fpr mode python."
   (bf-config-python--pyvenv)
+  (bf-config-python--pipenv)
+  (bf-config-python--company-jedi)
   nil)
 
 (provide 'bf-config-python)
