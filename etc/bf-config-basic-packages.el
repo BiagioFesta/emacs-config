ddb06225b815bfe630bc0c2b2bfd8d5ca3ab4ac6
BiagioFesta <biagio.festa@gmail.com>
2020-06-02 23:27:12 +0100

    Ivy count format with index and total

--- etc/bf-config-basic-packages.el
+++ etc/bf-config-basic-packages.el
@@ -65,6 +65,7 @@
     :init
     (setq ivy-use-virtual-buffers t)
     (setq enable-recursive-minibuffers t)
+    (setq ivy-count-format "(%d/%d) ")
     :config
     (ivy-mode 1))
   (use-package swiper
