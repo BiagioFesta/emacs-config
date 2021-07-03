932b8082840bf0b15be8293c206df55d110b127e
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-07-03 11:50:14 +0200

    general-packages: undo-tree

--- core/bf-config-general-packages.el
+++ core/bf-config-general-packages.el
@@ -18,10 +18,21 @@
     :config
     (which-key-mode 1)))
 
+(defun bf-config--general-packages--undo-tree ()
+  "Install and configure `undo-tree' package."
+  (use-package undo-tree
+    :ensure t
+    :diminish
+    :config
+    (global-undo-tree-mode 1)
+    :bind
+    ("C-c z" . undo-tree-visualize)))
+
 (defun bf-config--general-packages ()
   "Install and configure all general packages."
   (bf-config--general-packages--dimish)
-  (bf-config--general-packages--which-key))
+  (bf-config--general-packages--which-key)
+  (bf-config--general-packages--undo-tree))
 
 (provide 'bf-config-general-packages)
 ;;; bf-config-general-packages.el ends here
