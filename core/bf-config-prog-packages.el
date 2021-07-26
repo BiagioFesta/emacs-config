f0e9214228165a81be51cbfccea66c81400f0378
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-07-26 15:52:49 +0200

    prog-packages: ws-butler

--- core/bf-config-prog-packages.el
+++ core/bf-config-prog-packages.el
@@ -3,9 +3,19 @@
 ;;;   Install and configure programming packages.
 
 ;;; Code:
+(require 'use-package)
+
+(defun bf-config--prog-packages--ws-butler ()
+  "Install and configure package `ws-butler'."
+  (use-package ws-butler
+    :ensure t
+    :diminish
+    :hook
+    (prog-mode . ws-butler-mode)))
 
 (defun bf-config--prog-packages ()
-  "Install and configure all programming packages.")
+  "Install and configure all programming packages."
+  (bf-config--prog-packages--ws-butler))
 
 (provide 'bf-config-prog-packages)
 ;;; bf-config-prog-packages.el ends here
