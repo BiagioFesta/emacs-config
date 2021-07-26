597a4587aabc339b0b881c4e9a5b2ebb1882f00f
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-07-26 15:50:31 +0200

    prog-packages: empty configuration

---
+++ core/bf-config-prog-packages.el
@@ -0,0 +1,11 @@
+;;; bf-config-prog-packages.el  --- Biagio's Emacs Configuration
+;;; Commentary:
+;;;   Install and configure programming packages.
+
+;;; Code:
+
+(defun bf-config--prog-packages ()
+  "Install and configure all programming packages.")
+
+(provide 'bf-config-prog-packages)
+;;; bf-config-prog-packages.el ends here
