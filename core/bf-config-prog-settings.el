f88314da8e3493021272c4ffcfb31d1b595710b0
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-07-27 00:16:39 +0200

    prog-settings: disable indent with tabs

--- core/bf-config-prog-settings.el
+++ core/bf-config-prog-settings.el
@@ -5,7 +5,8 @@
 ;;; Code:
 
 (defun bf-config--prog-settings ()
-  "Apply all programming configuration settings.")
+  "Apply all programming configuration settings."
+  (setq-default indent-tabs-mode nil))
 
 (provide 'bf-config-prog-settings)
 ;;; bf-config-prog-settings.el ends here
