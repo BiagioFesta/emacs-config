e57f46f90cd54e5913065d3288351e1cf0db9e5b
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-08-05 11:56:21 +0200

    general-settings: global keybind for `display-line-numbers-mode`

--- core/bf-config-general-settings.el
+++ core/bf-config-general-settings.el
@@ -45,7 +45,8 @@ big buffers."
   "Set the global key bindings."
   (global-set-key (kbd "<f5>") 'revert-buffer)
   (global-set-key (kbd "C-\\") 'display-fill-column-indicator-mode)
-  (global-set-key (kbd "C-c \\") 'bf-config-utilities-column-indicator-at-point))
+  (global-set-key (kbd "C-c \\") 'bf-config-utilities-column-indicator-at-point)
+  (global-set-key (kbd "C-x \\") 'display-line-numbers-mode))
 
 (defun bf-config--general-settings--auto-revert-mode ()
   "Configure auto-revert minor mode."
