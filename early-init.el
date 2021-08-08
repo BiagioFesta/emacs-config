60c22b5349d5fdeae2da5a65a664faf81d07a7f6
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2021-08-08 20:28:43 +0200

    early-init: config scrolling

--- early-init.el
+++ early-init.el
@@ -14,10 +14,18 @@
   "Configure some apperances for the modeline."
   (column-number-mode 1))
 
+(defun bf-config--early-init--config-scroll ()
+  "Configure scrolling (mouse and keyboard)."
+ (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+ (setq mouse-wheel-progressive-speed nil)
+ (setq mouse-wheel-follow-mouse t)
+ (setq scroll-step 1))
+
 (defun bf-config--early-init ()
   "Launch early-init configuration."
   (bf-config--early-init--config-bars)
   (bf-config--early-init--config-modeline)
+  (bf-config--early-init--config-scroll)
   (setq inhibit-startup-screen t)
   (setq gc-cons-threshold (* 100 1024 1024))
   (setq read-process-output-max (* 10 1024 1024)))
