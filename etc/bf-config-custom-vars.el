4b823aebf23da6baea37aa90a1a6252cb0abf245
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2022-04-11 20:04:08 +0200

    config-custom-vars: some customs to commit

--- etc/bf-config-custom-vars.el
+++ etc/bf-config-custom-vars.el
@@ -7,7 +7,11 @@
 (require 'bf-config-vars)
 
 (defun bf-config--custom-vars ()
-  "Add custom variable settings here.")
+  "Add custom variable settings here."
+  (setq bf-config-general-packages-evil nil)
+  (setq bf-config-theme-with-display 'doom-palenight)
+  (setq bf-config-theme-with-console 'doom-palenight)
+  (setq bf-config-theme-font "Source Code Pro:slant=normal"))
 
 (provide 'bf-config-custom-vars)
 ;;; bf-config-custom-vars.el ends here
